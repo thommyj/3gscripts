3ff8c9e5e84e800713812d52690e71f513465827
Thommy Jakobsson <thommyj@gmail.com>
2018-06-05 00:41:01 +0200

    Use file descriptors
    If the device disapears the pipe will just create the file
    Also parse dmesg to find which tty we received
    
    Signed-off-by: Thommy Jakobsson <thommyj@gmail.com>

--- stayup.sh
+++ stayup.sh
@@ -1,60 +1,55 @@
 #!/bin/sh
-
+set -e
+set -x
 HOSTTOPING=8.8.8.8
 PINGSIZE=10
 PINGTIMEOUT=30
 PINGCOUNT=3
-SERIALTTY=/dev/ttyUSB0
-DONGLEMISSING=0
+SERIALTTY="none"
+LOGTAG="$0"
 
 ##run forever
-logger "$0 starting"
+logger -t $LOGTAG -s "$0 starting"
+loop=0
+fail=0
 while :
 do
-  if [ -c $SERIALTTY ]
-  then
-    DONGLEMISSING=0
-    #TODO: check if connected
-    #connect
+	if [ ! -z "$SERIALTTY" -a -c $SERIALTTY ]
+	then
+		loop=$((loop + 1))
+		echo -n "loop $loop, failed $fail"
+		PINGRESULT=`ping -w $PINGTIMEOUT -c $PINGCOUNT -s $PINGSIZE $HOSTTOPING | sed -e 's/, /\n/g' | grep "packet loss" | cut -f 1 -d "%"` 2>/dev/null
+		echo ", pingresult $PINGRESULT% loss"
 
-    PINGRESULT=`ping -w $PINGTIMEOUT -c $PINGCOUNT -s $PINGSIZE $HOSTTOPING | sed -e 's/, /\n/g' | grep "packet loss" | cut -f 1 -d "%"`
-    echo "Pingresult $PINGRESULT% loss"
-    #TODO: check signal quality
-    #TODO: check connection type
-
-    #disconnect if all packets where lost
-    if [ -z "$PINGRESULT" ] || [ $PINGRESULT -eq 100 ]
-    then
-      logger "WARNING: unable to ping google, reconnecting"
-      ##apperently sometimes the tty goes down, break out and wait
-      ##for it to comeback
-      if [ ! -c $SERIALTTY ]
-      then
-         continue
-      fi
-	
-      ##take down interface and reconnect
-      ifdown wwan0
-      echo -e "AT^NDISDUP=1,0,\"bredband.tre.se\"\r" > $SERIALTTY
-      sleep 1
-      echo -e "AT^NDISDUP=1,1,\"bredband.tre.se\"\r" > $SERIALTTY
-      ifup wwan0
-      ADDR=`ifconfig wwan0 | grep addr`
-      if [ -z "$ADDR" ]
-      then
-        logger "WARNING: no address received"
-      fi
-##      exit 1
-    else
-      #all ok
-      sleep 5
-    fi
-  else 
-    if [ $DONGLEMISSING -eq 0 ]
-    then
-      logger "no 3g dongle found!"
-      DONGLEMISSING=1
-    fi
-    sleep 5
-  fi
+		#disconnect if all packets where lost
+		if [ -z "$PINGRESULT" ] || [ $PINGRESULT -eq 100 ]
+		then
+			logger -t $LOGTAG -s "WARNING: unable to ping google, reconnecting"
+			fail=$((fail + 1))
+			ADDR=
+			while [ -z "$ADDR" ]
+			do
+				##take down interface and reconnect
+				ifdown wwan0
+				echo -e "AT^NDISDUP=1,0,\"bredband.tre.se\"\r" >&3 || continue
+				sleep 1
+				echo -e "AT^NDISDUP=1,1,\"bredband.tre.se\"\r" >&3 || continue
+				ifup wwan0
+				ADDR=`ifconfig wwan0 | grep "inet addr"`
+				logger -t $LOGTAG -s "received addr=$ADDR"
+				sleep 2
+			done
+		else
+			#all ok
+			sleep 5
+		fi
+	else 
+		sleep 1
+		exec 3>&-
+		SERIALTTY=`dmesg | grep -E "GSM modem.*attached" | tail -n 1 | sed -e 's/^.* attached to //g'`
+		SERIALTTY="/dev/${SERIALTTY}"
+		[ ! -z "$SERIALTTY" -a -c $SERIALTTY ] && exec 3<> $SERIALTTY && continue
+		logger -t $LOGTAG -s "no 3g dongle found!"
+		sleep 15
+	fi
 done
